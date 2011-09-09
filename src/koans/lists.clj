2d9dcb31d098c70ef36be834ecf0d072f61442a0
Eliot <fadookie@gmail.com>
2011-09-08 22:59:11 -0700

    Finished lists.clj

--- src/koans/lists.clj
+++ src/koans/lists.clj
@@ -15,20 +15,20 @@
   (= '(:a :b :c :d :e) (cons :a '(:b :c :d :e)))
 
   "Conjoining an element to a list can be done in the reverse order"
-  (= '(:a :b :c :d :e 0) (conj '(:a :b :c :d :e) 0))
+  (= '(0 :a :b :c :d :e) (conj '(:a :b :c :d :e) 0))
 
   "You can use a list like a stack to get the first element"
-  (= __ (peek '(:a :b :c :d :e)))
+  (= :a (peek '(:a :b :c :d :e)))
 
   "Or the others"
-  (= __ (pop '(:a :b :c :d :e)))
+  (= '(:b :c :d :e) (pop '(:a :b :c :d :e)))
 
   "But watch out if you try to pop nothing"
-  (= __ (try
+  (= "No dice!" (try
           (pop '())
           (catch IllegalStateException e "No dice!")))
 
   "The rest of nothing isn't so strict"
-  (= __ (try
+  (= '() (try
           (rest '())
           (catch IllegalStateException e "No dice!"))))
