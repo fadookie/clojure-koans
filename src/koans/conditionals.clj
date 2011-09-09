08e8318173d2db16d6eb055415e221cc07fdb082
Eliot <fadookie@gmail.com>
2011-09-09 01:11:40 -0700

    Finished conditionals... @_@

--- src/koans/conditionals.clj
+++ src/koans/conditionals.clj
@@ -9,39 +9,64 @@
 
 (meditations
   "You will face many decisions"
-  (= __ (if (false? (= 4 5))
+  (= :a (if (false? (= 4 5))
             :a
             :b))
 
+  "false? is a shortcut for (= false" ;) -Eliot
+  (= (false? false) (= false false))
+
   "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
+  (= [] (if (> 4 3)
             []))
 
   "And in such a situation you may have nothing"
-  (= __ (if (nil? 0)
+  (= nil (if (nil? 0)
              [:a :b :c]))
 
   "In others your alternative may be interesting"
   (= :glory (if (not (empty? ()))
                 :doom
-                __))
+                :glory))
+  ;Hey, (not x) is just like our old C friend !(x) :)
 
   "You may have a multitude of possible paths"
   (let [x 5]
-    (= :your_road (cond (= x __) :road_not_taken
-                        (= x __) :another_road_not_taken
-                        :else __)))
+    (= :your_road (cond (= x 6) :road_not_taken
+                        (= x 7) :another_road_not_taken
+                        :else :your_road)))
+
+;Check out the differences inside of a let due to '() style list specification (delayed evaluation...?)
+;(let    [x 1
+;        y 2]
+;'(x y))
+;evals to (x y)
+
+;(let    [x 1
+;        y 2]
+;(list x y))
+;evals to (1 2)
+
+;((let    [x 1
+;        y 2]
+;#(list x y)))
+;evals to (1 2)
 
   "Or your fate may be sealed"
-  (= __ (if-not (zero? __)
+  (= 'doom (if-not (zero? 0)
                 'doom
                 'doom))
 
+  "Or not" ;) -Eliot
+  (= 'boom (if-not (zero? 0)
+                'doom
+                'boom))
+
   "In case of emergency, sound the alarms"
   (= :sirens
-     (explain-defcon-level __))
+     (explain-defcon-level :cocked-pistol))
 
   "But admit it when you don't know what to do"
-  (= __
+  (= :say-what?
      (explain-defcon-level :yo-mama)))
 
